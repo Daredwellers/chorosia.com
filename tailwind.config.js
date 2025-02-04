2e8b135c007533795e9a1acec272b4421e6b4930
Anto Pranjić <anto.pranjic@runevents.eu>
2025-02-04 18:34:20 +0100

    First actual try

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: [
+    './src/**/*.{astro,html,js,jsx,ts,tsx}', // Include all files in the src directory
+    './components/**/*.{astro,html,js,jsx,ts,tsx}', // Include all files in the components directory
+    './layouts/**/*.{astro,html,js,jsx,ts,tsx}', // Include all files in the layouts directory
+    './pages/**/*.{astro,html,js,jsx,ts,tsx}', // Include all files in the pages directory
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
