cf30666924da17866aea5133a0e58b0e17bcd227
Daredwellers <chorosia.band@gmail.com>
2025-02-09 00:18:15 +0100

    saving just in case

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,7 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import tailwind from '@astrojs/tailwind';
+import markdownIntegration from '@astropub/md'
 
 // https://astro.build/config
 export default defineConfig({
@@ -9,8 +10,15 @@ export default defineConfig({
     integrations: [
         mdx(),
         sitemap(),
+        markdownIntegration(),
         tailwind({
             applyBaseStyles: false
         })
-    ]
+    ],
+    markdown: {
+        remarkPlugins: [],
+        rehypePlugins: [],
+        // syntaxHighlight: 'shiki'
+        // syntaxHighlight: 'prism'
+      }
 });
